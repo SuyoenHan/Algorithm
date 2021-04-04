3a143dc2b704e790c041ce4db2ec1858feed2c95
SuyeonHan <syflowerdaba@gmail.com>
2021-04-04 18:38:24 +0900

    20210404_3차풀이(41% 런타임에러)

--- Algorithm/Programmers/level1/level1_8.java
+++ Algorithm/Programmers/level1/level1_8.java
@@ -14,15 +14,16 @@ public class level1_8 {
 		// 체육수업을 들을 수 있는 학생의 최댓값을 구하라
 		
 			// n, lost, reserve 변수의 값은 가변적
-			int n=5;  // 전체 학생 수 n
-			int[] lost=new int[] {2,4}; // 체육복을 도난당한 학생 번호
-			int[] reserve= new int[] {1,3,5}; // 여벌체육복이 있는 학생 번호
+			int n=10;  // 전체 학생 수 n
+			int[] lost=new int[] {5,7,9}; // 체육복을 도난당한 학생 번호
+			int[] reserve= new int[] {1,2,3,4,6,8}; // 여벌체육복이 있는 학생 번호
 			
 			int answer=0;
 			List<Integer> reserveList = new ArrayList<>();
 			List<Integer> lostList = new ArrayList<>();
-						
 			
+			
+			// reserve 배열 중 도난당하지 않은 학생들 reserveList에 넣어주기 => 체육복을 빌려줄 수 있는 학생 리스트 생성
 			for(int i=0;i<reserve.length;i++) {
 				reserveList.add(reserve[i]);
 				
@@ -34,9 +35,19 @@ public class level1_8 {
 			}// end of for------
 
 			
+			// lost 배열 중 여벌의 체육복이 있는 학생들을 제외한 학생들 lostList에 넣어주기 => 체육복을 빌려야만 하는 학생 리스트 생성
 			for(int i=0;i<lost.length;i++) {
-				lostList.add(lost[i]);
-			}
+				boolean flag=false; // 초기화
+				for(int j=0;j<reserve.length;j++) {
+					if(lost[i]==reserve[j]) {
+						flag=true;
+						break;
+					}
+				}
+				if(!flag) {
+					lostList.add(lost[i]);
+				}
+			}// end of for-------
 			
 			// 앞에서 뒤로 빌려주기 => List의 remove는 인덱스 신경써야 한다!!
 			
