07c015f69dfe21f573e2070d3d0bdf85e14e5934
SuyeonHan <syflowerdaba@gmail.com>
2021-04-04 18:15:04 +0900

    20210404_2차풀이(33% 런타임에러)

--- Algorithm/Programmers/level1/level1_8.java
+++ Algorithm/Programmers/level1/level1_8.java
@@ -19,115 +19,48 @@ public class level1_8 {
 			int[] reserve= new int[] {1,3,5}; // 여벌체육복이 있는 학생 번호
 			
 			int answer=0;
-			boolean flag= false;
-			List<Integer> arrList = new ArrayList<>(); 
-			List<Integer> arrList2 = new ArrayList<>(); 
 			List<Integer> reserveList = new ArrayList<>();
-			List<Integer> reserveList2 = new ArrayList<>();
 			List<Integer> lostList = new ArrayList<>();
-			List<Integer> lostList2 = new ArrayList<>();
+						
 			
-			// lost배열에 있지 않은 학생들 arrList에 추가
-			for(int i=0;i<n;i++) {
-				flag= false; // 초기화
-				
-				for(int j=0;j<lost.length;j++) {
-					if(i+1 == lost[j]) {
-						flag= true;
-						break;
-					}
-				}
-		
-				if(!flag) {
-					arrList.add(i+1);
-					arrList2.add(i+1);
-				}
-			}// end of for--------
-			
-			
-			// lost 배열에 있지만 여벌의 체육복이 있는 학생 arrList에 추가  & reserveList에서 제거
 			for(int i=0;i<reserve.length;i++) {
 				reserveList.add(reserve[i]);
-				reserveList2.add(reserve[i]);
 				
 				for(int j=0;j<lost.length;j++) {
 					if(reserve[i]==lost[j]) {
-						arrList.add(reserve[i]);
-						arrList2.add(reserve[i]);
 						reserveList.remove(i);
-						reserveList2.remove(i);
 					}
 				}
 			}// end of for------
 
-
-			for(int i=0; i<lost.length;i++) {
+			
+			for(int i=0;i<lost.length;i++) {
 				lostList.add(lost[i]);
-				lostList2.add(lost[i]);
 			}
 			
-			// 앞 => 뒤 
+			// 앞에서 뒤로 빌려주기 => List의 remove는 인덱스 신경써야 한다!!
 			
-			for(int i=0;i<lostList2.size();i++) {
-				for(int j=0;j<reserveList2.size();j++) {
-					if(lostList2.get(i)-1 == reserveList2.get(j)) {
-						arrList.add(lostList2.get(i));
-						reserveList2.remove(j);
-						lostList2.remove(i);
-						break;
-					}
-				}
-			}
-			
-			for(int i=0;i<lostList2.size();i++) {
-				for(int j=0;j<reserveList2.size();j++) {
-					if(lostList2.get(i)+1 == reserveList2.get(j)) {
-						arrList.add(lostList2.get(i));
-						reserveList2.remove(j);
-						lostList2.remove(i);
-						break;
-					}
-				}
-			}
-			
-			int len_case1= arrList.size();
-			// System.out.println(len_case1);
-			
-			
-			// 뒤 => 앞
-	
 			for(int i=0;i<lostList.size();i++) {
 				for(int j=0;j<reserveList.size();j++) {
 					if(lostList.get(i)-1 == reserveList.get(j)) {
-						arrList2.add(lostList.get(i));
 						reserveList.remove(j);
 						lostList.remove(i);
+						j--;
+						i--;
+						break;
+					}
+					else if(lostList.get(i)+1 == reserveList.get(j)) {
+						reserveList.remove(j);
+						lostList.remove(i);
+						j--;
+						i--;
 						break;
 					}
 				}
-			}
+			} // end of for-------------
 			
-			for(int i=0;i<lostList.size();i++) {
-				for(int j=0;j<reserveList.size();j++) {
-					if(lostList.get(i)+1 == reserveList.get(j)) {
-						arrList2.add(lostList.get(i));
-						reserveList.remove(j);
-						lostList.remove(i);
-						break;
-					}
-				}
-			}
 			
-			int len_case2= arrList2.size();
-			// System.out.println(len_case2);
-			
-	
-			if(len_case1>=len_case2) {
-				answer= len_case1;
-			}
-			else if(len_case1<len_case2) {
-				answer= len_case2;
-			}
+			answer= n-(lostList.size());
 			
 			System.out.println(answer);
 			
