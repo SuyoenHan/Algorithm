f2f3112c38e2a36612fe979ce518f6f2107778f7
SuyeonHan <syflowerdaba@gmail.com>
2021-04-09 21:08:48 +0900

    20210409_1차풀이(94%)

---
+++ Algorithm/Programmers/level1/Level1_35.java
@@ -0,0 +1,34 @@
+package level1;
+
+public class Level1_35 {
+	
+	// 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/courses/30/lessons/12934 
+	// 문제명: 정수 제곱근 판별
+		
+	public static void main(String[] args) {
+	
+		// 양의 정수 n에 대해 n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하기
+		// n이 양의 정수 x의 제곱이라면 x+1제곱을 출력하고, n이 양의 정수 x의 제곱이 아니라면 -1을 출력해라
+	
+		long n= 1;
+		long answer=0;
+		
+		double nSqrt= Math.sqrt(n);
+		String sSqrt= String.valueOf(nSqrt);
+		System.out.println(sSqrt);
+		
+		int index= sSqrt.indexOf(".");
+		System.out.println(index);
+		
+		
+		if( (int) sSqrt.charAt(index+1) == (int)'0') {
+			answer= (long)((nSqrt+1)*(nSqrt+1)); 
+		}
+		else {
+			answer=-1;
+		}
+	
+		System.out.println(answer);
+	}// end of main ----------------------
+	
+}
