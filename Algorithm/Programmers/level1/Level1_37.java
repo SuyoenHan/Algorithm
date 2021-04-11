f9d85f6b34de6691681477e764a86199e6c1bb26
SuyeonHan <syflowerdaba@gmail.com>
2021-04-11 13:44:21 +0900

    20210411_2차풀이(100% 효율성 개선 완료)

--- Algorithm/Programmers/level1/Level1_37.java
+++ Algorithm/Programmers/level1/Level1_37.java
@@ -10,22 +10,27 @@ public class Level1_37 {
 		// 1부터 입력받은 숫자 n 사이에 있는 소수의 개수 구하기 
 		// 소수는 1과 자기 자신으로만 나누어지는 수를 의미하며 1은 소수가 아니다
 		
-		int n=5;
-		int answer=0;
-		int cnt=1; // cnt (소수가 아닌 수) ==> 1 포함
+		// 효율성 개선을 위해 에라토스테네스의 체 방법 이용 => O(n)
+		/* 2 이외의 2의 배수를 지우고, 3 이외의 3의 배수를 지우고, 5이외의 5의 배수를 지우고 .... */
 		
-		for(int i=2;i<=n;i++) {
-			for(int j=2;j<=i/2;j++) {
-				if(i%j==0) { // 소수가 아닌인경우
-					cnt++;
-					break;
-				}
+		int n=10;
+		int answer=-1;  // 1은 소수가 아니므로 
+		boolean[] primeArray= new boolean[n]; // default값은 false
+		
+		for(int i=2;i*i<n;i++) {
+			for(int j=2;i*j<=n;j++) {
+				primeArray[(i*j)-1]=true;
+				    //인덱스번호와 값 일치시키기 위함
 			}
-		} // end of for---
+		}
 		
-		answer= n-cnt;
+		for(int i=0;i<primeArray.length;i++) {
+			if(primeArray[i]== false) {
+				answer++;
+			}
+		}
+		
 		System.out.println(answer);
-		
 		
 	}// end of main ----------------------
 	
